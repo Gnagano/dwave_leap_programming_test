9a2255cc75a7ef28621f0a3fd20736fd11d4070b
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-10-11 16:42:23 +0800

    coded 001dwave_cloud.py. work well

--- sample/001dwave_cloud.py
+++ sample/001dwave_cloud.py
@@ -1,3 +1,10 @@
+import sys
+sys.path.append('..')
+
+from config import account
 from dwave.cloud import Client
-client = Client.from_config(token='DEV-c7a62b6fb390e91e63c6ac2a7f8450fa50a6d09d')
+
+client = Client.from_config(token=account.LEAP_API_TOKEN)
 print client.get_solvers()
+
+#print account.LEAP_API_TOKEN
