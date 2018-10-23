38f00457c1339d38af85b84448d40bad16f126b4
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-10-23 10:33:39 +0800

    compare the computing power between anealing and cpu

--- sample/019vetex_cover/021wheel_graph_classical.py
+++ sample/019vetex_cover/021wheel_graph_classical.py
@@ -1,9 +1,19 @@
 import networkx       as nx
 import dwave_networkx as dnx
 from dimod.reference.samplers import ExactSolver
+import datetime
+
+start = datetime.datetime.now()
+print "start time is " + str(start)
 
 sampler = ExactSolver()
-w5 = nx.wheel_graph(5)
-#w25 = nx.wheel_graph(25)
-print(dnx.min_vertex_cover(w5,sampler))
-#print(dnx.min_vertex_cover(w25,sampler))
+
+# w5 = nx.wheel_graph(5)
+# print(dnx.min_vertex_cover(w5,sampler))
+
+w25 = nx.wheel_graph(23)
+print(dnx.min_vertex_cover(w25,sampler))
+
+end = datetime.datetime.now()
+print "the end time is " + str(end)
+print "the calculate time is " + str(end - start)
--- sample/019vetex_cover/021wheel_graph_dwave.py
+++ sample/019vetex_cover/021wheel_graph_dwave.py
@@ -5,17 +5,24 @@ from config import account
 from config import config
 import networkx       as nx
 import dwave_networkx as dnx
-
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
+import datetime
 
 sampler = EmbeddingComposite(DWaveSampler(
     endpoint = config.API_ENDPOINT,
     token    = account.LEAP_API_TOKEN,
     solver   = config.SOLVER_NAME))
 
-w5 = nx.wheel_graph(5)
-w25 = nx.wheel_graph(25)
+start = datetime.datetime.now()
+print "start time is " + str(start)
 
-print(dnx.min_vertex_cover(w5,sampler))
+# w5 = nx.wheel_graph(5)
+# print(dnx.min_vertex_cover(w5,sampler))
+
+w25 = nx.wheel_graph(25)
 print(dnx.min_vertex_cover(w25,sampler))
+
+end = datetime.datetime.now()
+print "the end time is " + str(end)
+print "the calculate time is " + str(end - start)
